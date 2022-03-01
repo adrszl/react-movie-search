fbd80f5bfe4232724c65ebf884224221aa274177
Adrian Szlegel <adrian.szlegel@hotmail.com>
2022-03-01 15:46:27 +0100

    created new section for advanced search parameters and started functionality for it

--- src/assets/additionalInfo.js
+++ src/assets/additionalInfo.js
@@ -1,4 +1,7 @@
 export const additionalInfo = {
   CHANGE_LOG: `28.10.2021 --- added footer, modal, changelog. Changes in layout, added new antd layout components. 
-  Added spaces between rows in results.`
+  Added spaces between rows in results.
+  
+  01.03.2022 --- added custom styles for cards and spacing. Changes in functionality (improvements). Added advanced
+  parameters section and started functionality for it.`
 };
--- src/components/Search.js
+++ src/components/Search.js
@@ -1,26 +1,54 @@
 import React, { useState } from "react";
 
 // ANT DESIGN
-import { Form, Input, Button, Row, Col } from "antd";
+import { Form, Input, Button, Row, Col, Checkbox, Divider, Menu, Dropdown } from "antd";
 
 const Search = (props) => {
   const [searchValue, setSearchValue] = useState("");
+  const [showAdvanced, setShowAdvancedValue] = useState(false);
+  const [typeValue, setTypeValue] = useState("Type");
 
   const handleSearchInputChanges = (e) => {
     setSearchValue(e.target.value);
   };
 
-  const resetInputField = () => {
-    setSearchValue("");
-  };
+  const handleCheckboxChange = () => {
+    setShowAdvancedValue(!showAdvanced);
+  }
+
+  // const handleTypesClick = () => {
+    
+  // }
+
+  const handleDropdownClick = () => {
+
+  }
+
+  // const resetInputField = () => {
+  //   setSearchValue("");
+  // };
 
   const callSearchFunction = (e) => {
     e.preventDefault();
     props.search(searchValue);
-    resetInputField();
   };
 
+  const types = (
+    <Menu>
+      <Menu.Item key="1" onClick={() => setTypeValue("Movie")}>
+        Movie
+      </Menu.Item>
+      <Menu.Item key="2" onClick={() => setTypeValue("Series")}>
+        Series
+      </Menu.Item>
+      <Menu.Item key="3" onClick={() => setTypeValue("Episode")}>
+        Episode
+      </Menu.Item>
+    </Menu>
+  );
+
   return (
+    <>
     <Form.Item
       label="Search for movies"
       required
@@ -32,6 +60,7 @@ const Search = (props) => {
             placeholder="eg. Iron Man"
             value={searchValue}
             onChange={handleSearchInputChanges}
+            onPressEnter={callSearchFunction}
             type="text"
           />
         </Col>
@@ -45,8 +74,31 @@ const Search = (props) => {
           </Button>
         </Col>
       </Row>
+      <Col span={12} className="mt-4">
+          <Checkbox onChange={handleCheckboxChange}>Show Advanced Search Parameters</Checkbox>
+      </Col>
       <Col className="gutter-row" flex="row"></Col>
     </Form.Item>
+    {showAdvanced ?
+      <>
+        <Divider orientation="center">Advanced Search Parameters</Divider>
+          <p>Functionality not yet finished. To be continued</p>
+          <Row justify="center">
+          <Col span={6}>
+            <Input
+              placeholder="Year"
+              type="number"
+            />
+          </Col>
+          <Col span={6}>
+          <Dropdown.Button onClick={handleDropdownClick} overlay={types} trigger={['click']}>
+            {typeValue}
+          </Dropdown.Button>
+          </Col>
+          </Row>
+      </>
+    : null }
+    </>
   );
 };
 
